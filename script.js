edbd546bbbaf57d0bb61410479657f4260adb6e9
Manoj <117893476+ManojAcharya27@users.noreply.github.com>
2023-07-12 17:36:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,18 @@
 function fibonacci(num) {
 // your code here
-	if(n<=2) return num;
-	let first=0;
-	let second=1;
-	let ans=0;
-	for(let i=2;i<num;i++){
-		ans=first+second;
-		first=second;
-		second=ans;
-	}
-	return ans;
+	if (num <= 1) {
+    return num;
+  }
+
+  const fib = new Array(num + 1);
+  fib[0] = 0;
+  fib[1] = 1;
+
+  for (let i = 2; i <= num; i++) {
+    fib[i] = fib[i - 1] + fib[i - 2];
+  }
+
+  return fib[num];
 }
 
 module.exports = fibonacci;
